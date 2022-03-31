254d73fbef74a9105f7d125dc4c422ccf0a0cd04
PHBasin <60905941+PHBasin@users.noreply.github.com>
2022-03-31 15:52:25 +0200

    update testcase.sh

--- scripts/testcase.sh
+++ scripts/testcase.sh
@@ -5,8 +5,8 @@ read response
 
 case $response in
 	"y" | "yes" )		echo "I hop you know what your are doing!";
-									echo "I am supposed to type: rm -rf /";;
+				echo "I am supposed to type: rm -rf /";;
 	"n" | "no" )		echo "You have comon sens! Aborting..";;
-	* )		echo "You have to give an answer!";;
+	* )			echo "You have to give an answer!";;
 esac
 exit 0
